85790782de4cb09f7fe1d4b15fc1b02ad71a7647
Julien Lirochon <julien@lirochon.net>
2014-01-24 22:23:51 +0100

    on n'active pas le mode debug en prod !

--- komuso/settings.py
+++ komuso/settings.py
@@ -19,9 +19,9 @@ BASE_DIR = os.path.realpath(os.path.dirname(__file__) + '/..')
 SECRET_KEY = '4+_55w=d2&&v$^fs7gns7(d9q#9f_wtsz-7rf!juyi1vc!v7d8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = DEBUG
 
 ALLOWED_HOSTS = []
 
